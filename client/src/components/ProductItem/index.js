55c2f084f4e7209039fb0c8658247081df5f7a31
Shay Wagner <srwagner916@gmail.com>
2021-07-21 16:34:24 -0500

    implemented indexedDb and item persistence

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
@@ -25,6 +26,7 @@ function ProductItem(item) {
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 }
       });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   };
   
